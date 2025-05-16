8101c9c919d6d2f453db66959be156125fac4379
Jonny Comes <jcomes@collegeofidaho.edu>
2025-05-16 08:38:41 -0600

    Fixed PK reference in index creation

--- data/sql/create_indexes.sql
+++ data/sql/create_indexes.sql
@@ -2,7 +2,7 @@ CREATE INDEX IF NOT EXISTS idx_movie_id ON movie(movie_id);
 CREATE INDEX IF NOT EXISTS idx_person_id ON person(person_id);
 CREATE INDEX IF NOT EXISTS idx_movielens_id ON movie_link(movielens_id);
 CREATE INDEX IF NOT EXISTS idx_movie_link_tmdb_id ON movie_link(tmdb_id);
-CREATE INDEX IF NOT EXISTS idx_user_movie_rating_movielens_id ON user_movie_rating(movielens_id);
+CREATE INDEX IF NOT EXISTS idx_user_movie_rating_movielens_id ON user_movie_rating(movie_id);
 CREATE INDEX IF NOT EXISTS idx_user_movie_rating_rating ON user_movie_rating(rating);
 CREATE INDEX IF NOT EXISTS idx_mg_movie_id ON movie_genre(movie_id);
 CREATE INDEX IF NOT EXISTS idx_mg_genre_id ON movie_genre(genre_id);
