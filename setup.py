0d06d39a9f66a8674320fef62f58dd8a8b809102
Jonny Comes <jcomes@collegeofidaho.edu>
2025-05-15 23:16:10 -0600

    Modified setup to handle lightfm issue

--- setup.py
+++ setup.py
@@ -3,20 +3,22 @@ from setuptools import setup, find_packages
 setup(
     name='movie_project',
     version='0.1',
-    packages=find_packages(where='src'), 
-    package_dir={'': 'src'}, 
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
     install_requires=[
-        'pandas', 
-        'seaborn', 
-        'matplotlib', 
-        'scikit-learn', 
-        'numpy',  
-        'lightfm', 
+        'pandas',
+        'seaborn',
+        'matplotlib',
+        'scikit-learn',
+        'numpy',
     ],
+    extras_require={
+        'recommender': ['lightfm'],
+    },
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License', 
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6', 
+    python_requires='>=3.6',
 )
